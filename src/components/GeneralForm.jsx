8c526b1b6e91769e8c4e06bbccce8a771504ca5d
Tyler Hudson <tylerhudson96@gmail.com>
2023-11-08 16:53:02 -0500

    Form capturing basic user data

---
+++ src/components/GeneralForm.jsx
@@ -0,0 +1,65 @@
+import SectionControl from "./SectionControl";
+
+function GeneralForm({ formData, onChange }) {
+  return (
+    <>
+      <form className="mw-50">
+        <div className="form-group">
+          <label htmlFor="firstNameInput">First Name *</label>
+          <input
+            type="email"
+            name="firstName"
+            value={formData.firstName}
+            className="form-control"
+            id="firstNameInput"
+            onChange={onChange}
+            placeholder="Jane/John"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastNameInput">Last Name *</label>
+          <input
+            type="email"
+            name="lastName"
+            value={formData.lastName}
+            className="form-control"
+            id="lastNameInput"
+            onChange={onChange}
+            placeholder="Doe"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="emailInput">Email Address *</label>
+          <input
+            type="email"
+            name="lastName"
+            value={formData.email}
+            className="form-control"
+            id="emailInput"
+            onChange={onChange}
+            placeholder="example@email.com"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phoneInput">Phone Number *</label>
+          <input
+            type="email"
+            name="lastName"
+            value={formData.phone}
+            className="form-control"
+            id="phoneInput"
+            onChange={onChange}
+            placeholder="555-123-9876"
+            required
+          />
+        </div>
+      </form>
+      <SectionControl />
+    </>
+  );
+}
+
+export default GeneralForm;
